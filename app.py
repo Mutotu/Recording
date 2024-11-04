c3c4e2b596bc1702f08a26aa2ed40cd90a36e394
Muttalip Aydemir <muttalip.aydemir@gmail.com>
2024-11-04 13:03:57 +0000

    added error andling with abort from flask_smorest

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 from db import items, stores
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 
 @app.get("/store")
@@ -13,6 +14,11 @@ def get_stores():
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(400, message="Bad request. Ensure 'name' is included in the JSON payload.")
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message="Store already exists.")
     store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id}
     stores[store_id] = store
@@ -21,8 +27,13 @@ def create_store():
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
+    if "price" not in item_data or "store_id" not in item_data or "name" not in item_data:
+        abort(404, message="Bad request. Ensure 'price', 'store_id' and 'name' are included in the JSON payload")
+    for item in items.values():
+        if item_data["name"] == item["name"] and item_data["store_id"] == item["store_id"]:
+            abort(400, message=f"Store not found")
     if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found")
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
     items[item_id] = item
@@ -38,11 +49,11 @@ def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found")
 
 @app.get("/item/<string:item_id>")
 def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"message": "Item not found"}, 404
+        abort(404, message="Item not found")
